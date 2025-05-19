77ac2a17ed3b2f128b777f39ceed1e7151557d40
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-05-19 21:22:20 +0800

    Add update and delete employee endpoints

--- server/src/routes/employeeRoutes.js
+++ server/src/routes/employeeRoutes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { listEmployees, createEmployee } from '../controllers/employeeController.js';
+import {
+  listEmployees,
+  createEmployee,
+  updateEmployee,
+  deleteEmployee
+} from '../controllers/employeeController.js';
 
 const router = Router();
 
 router.get('/', listEmployees);
 router.post('/', createEmployee);
+router.put('/:id', updateEmployee);
+router.delete('/:id', deleteEmployee);
 
 export default router;
