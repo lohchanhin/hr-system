f90ee43b7fcffd760b5fb5da0643683700af34e8
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-05-20 03:13:01 +0800

    fix pinia init

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 // src/main.js
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
@@ -7,6 +8,8 @@ import 'element-plus/dist/index.css'
 import './assets/main.css'
 
 const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
