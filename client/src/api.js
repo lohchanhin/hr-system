331f2598e467dc43a92f9fe25f72ebf109c2b9d1
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-08-25 17:09:13 +0800

    test: cover apiFetch token expiry

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-import { getToken } from './utils/tokenService'
+import { getToken, clearToken } from './utils/tokenService'
 
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3000'
@@ -9,5 +9,11 @@ export function apiFetch(path, options = {}) {
     ...(options.headers || {}),
     ...(token ? { Authorization: `Bearer ${token}` } : {})
   }
-  return fetch(`${API_BASE_URL}${path}`, { ...options, headers })
+  return fetch(`${API_BASE_URL}${path}`, { ...options, headers }).then(res => {
+    if (res.status === 401) {
+      clearToken()
+      window.location.href = '/login'
+    }
+    return res
+  })
 }
