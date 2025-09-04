6083091e1d77ed65a7f68d46ebf8bb5b55e398a5
lohchanhin <chanhin17@gmail.com>
2025-09-04 14:34:16 +0800

    u

--- client/dist/assets/menu-CCaJv1UL.js
+++
@@ -1,1 +0,0 @@
-import{A as o,r as s}from"./index-D6rWx2Du.js";import{a as r}from"./api-CHawSdM-.js";const c=o("menu",()=>{const t=s([]);async function n(){const e=await r("/api/menu");e.ok&&(t.value=await e.json())}function a(e){t.value=e}return{items:t,fetchMenu:n,setMenu:a}});export{c as u};
