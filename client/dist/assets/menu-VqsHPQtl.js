f1cb8176efe52afc27f0689f12122820dab80559
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-10-27 21:25:34 +0800

    u

--- client/dist/assets/menu-VqsHPQtl.js
+++
@@ -1,1 +0,0 @@
-import{B as o,r as s}from"./index-B6oJpjLF.js";import{a as r}from"./api-C-QqRWhG.js";const c=o("menu",()=>{const t=s([]);async function n(){const e=await r("/api/menu");e.ok&&(t.value=await e.json())}function a(e){t.value=e}return{items:t,fetchMenu:n,setMenu:a}});export{c as u};
