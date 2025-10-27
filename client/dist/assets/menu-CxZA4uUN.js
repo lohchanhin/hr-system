221949484f7e786c3334587b447af12926741000
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2025-10-27 21:49:41 +0800

    u

---
+++ client/dist/assets/menu-CxZA4uUN.js
@@ -0,0 +1,1 @@
+import{B as o,r as s}from"./index-Bc12ixp6.js";import{a as r}from"./api-BuNyVVPZ.js";const c=o("menu",()=>{const t=s([]);async function n(){const e=await r("/api/menu");e.ok&&(t.value=await e.json())}function a(e){t.value=e}return{items:t,fetchMenu:n,setMenu:a}});export{c as u};
