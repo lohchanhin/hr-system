3ca0d984dfd25e9b5685407e3fffa9576e55214b
盧振興 <57373012+lohchanhin@users.noreply.github.com>
2026-03-26 00:02:21 +0800

    重構排班格元件並優化單格渲染資料快取

---
+++ client/src/views/front/ScheduleCellEditor.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-select
+    v-model="scheduleCell.shiftId"
+    placeholder="選擇班別"
+    class="cell-select shift-select"
+    size="small"
+    @change="val => $emit('select-shift', val)"
+  >
+    <el-option
+      v-for="opt in shifts"
+      :key="opt._id"
+      :label="formatShiftLabel(opt)"
+      :value="opt._id"
+    />
+  </el-select>
+</template>
+
+<script setup>
+defineProps({
+  scheduleCell: { type: Object, required: true },
+  shifts: { type: Array, default: () => [] },
+  formatShiftLabel: { type: Function, required: true }
+})
+
+defineEmits(['select-shift'])
+</script>
